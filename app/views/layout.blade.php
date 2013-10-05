8051dff073a9b44354baaf7c541f56968848f93f
Vitaly Serov <serovvitaly@gmail.com>
2013-10-06 00:14:09 +0600

    очередной коммит

---
+++ app/views/layout.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title>Untitled</title>
+  
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+  <!--script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script-->
+  <script src="/packages/jquery/jquery-tmpl/jquery.tmpl.min.js"></script>
+  <script src="/packages/bootstrap/js/bootstrap.min.js"></script>
+  
+  <link rel="stylesheet" type="text/css" href="/packages/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="/skins/base/css/style.css">
+</head>
+<body>
+  <div class="container">
+    @yield('content')
+  </div>
+</body>
+</html>
---
+++ app/views/search.blade.php
@@ -0,0 +1,139 @@
+@extends('layout')
+
+@section('content') 
+
+<div class="row">
+  <div class="span12">поиск</div>
+  <div class="span10">
+    <div id="searh-result">
+      <table class="results rounded white table table-bordered table-striped table-hover">
+        <thead><tr>
+          <th style="width: 100px;">Дата заезда</th>
+          <th style="width: 50px;">Ночей</th>
+          <th>Отель</th>
+          <th>Курорт</th>
+          <th style="width: 160px;">Питание</th>
+          <th style="width: 100px;">Размещение</th>
+          <th style="width: 100px;">Стоимость</th>
+          <th style="width: 50px;">Места</th>
+        </tr></thead>
+        <tbody>
+       
+        </tbody>
+      </table>
+      
+      <div style="text-align: center; margin: 0 0 20px;">
+        <button style="display: none;" class="btn" id="search-result-load-button" onclick="showResults(10)">Показать еще</button>
+      </div>
+        
+    </div>  
+  </div>
+  <div class="span2">
+    <div class="rounded white" style="margin-left: -20px;">
+      ФИЛЬТР
+    </div>
+  </div>
+</div>
+
+<script>
+
+var searchResult = [];
+var searchResultStart = 0;
+
+function showHotelInfo(hotelId){
+    alert(hotelId);
+}
+
+function showBookingDialog(){
+    alert('showBookingDialog');
+}
+
+function showResults(count){
+    var limit = 0;
+    
+    if (count && count > 0) limit = searchResultStart + count;
+    if (limit > searchResult.length) limit = searchResult.length;
+    
+    if (limit < 1) {
+        return false;
+    }
+    
+    if (searchResultStart >= limit) {
+        $('#search-result-load-button').hide();
+        return false;
+    }
+    $('#search-result-load-button').show();
+        
+    for (var i = searchResultStart; i < limit; i++) {
+        //console.log(searchResult[i]);
+        var rowItem = $.tmpl($('#tpl-search-result-item'), searchResult[i]);
+        
+        var popover = rowItem.find('.sr-popover');
+        
+        rowItem.find('.hover')
+            .on('mouseover', function(e){
+                popover.show();
+            })
+            .on('mouseout', function(e){
+                popover.hide();
+            });
+        
+        rowItem.appendTo('#searh-result .results tbody');
+    }
+    
+    searchResultStart = i;
+}
+
+$(document).ready(function(){
+    $.ajax({
+        url: 'http://service.appros/tez/search',
+        dataType: 'jsonp',
+        type: 'GET',
+        success: function(data){
+            if (data.success === true) {
+                searchResult = data.result;
+                
+                showResults(20);
+            }
+        }
+    });
+});
+</script>
+
+<script id="tpl-search-result-item" type="text/x-jquery-tmpl">
+<tr>
+  <td>${checkIn}</td>
+  <td>${nightCount}</td>
+  <td><strong><a href="#" onclick="showHotelInfo(${hotelId}); return false;">${hotel}</a></strong><br><span class="font-gray font-small">${hotelRoomType}</span></td>
+  <td>${region}<br><span class="font-gray font-small">${tour}</span></td>
+  <td>${pansionComment} (${pansion})</td>
+  <td>${ageGroupType} (${hotelStayType})</td>
+  <td class="price"><a href="" onclick="showBookingDialog(); return false;">${price}</a></td>
+  <td class="hover">${existsRoom}
+    <div class="sr-popover">
+      <div class="sr-row sr-header">
+        <div class="title">Билеты</div>
+        <div>Туда</div>
+        <div>Обратно</div>
+      </div>
+      <div class="sr-row">
+        <div class="title">Эконом</div>
+        <div>${freeSeatNumberToY}</div>
+        <div>${freeSeatNumberFromY}</div>
+      </div>
+      <div class="sr-row">
+        <div class="title">Премиум-Эконом</div>
+        <div>${freeSeatNumberToR}</div>
+        <div>${freeSeatNumberFromR}</div>
+      </div>
+      <div class="sr-row">
+        <div class="title">Бизнес</div>
+        <div>${freeSeatNumberToC}</div>
+        <div>${freeSeatNumberFromC}</div>
+      </div>
+    </div>
+  </td>
+</tr>
+</script>
+
+@endsection
