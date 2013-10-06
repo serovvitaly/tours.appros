a6cc9f27954b8cda9dfc5456b4a95860074a7073
Vitaly Serov <serovvitaly@gmail.com>
2013-10-07 00:28:13 +0600

    очередной коммит

--- app/views/filter.blade.php
+++ app/views/filter.blade.php
@@ -9,22 +9,22 @@
 <!-- -->
       <div>
         Откуда:<br/>
-        <input type="text" style="width: 166px">
+        <input type="text" style="width: 165px">
         Куда:<br/>
-        <input type="text" style="width: 166px">
+        <input type="text" style="width: 165px">
       </div>
       <div>
         Диапазон дат заезда:<br/>
         <input name="after" type="text" class="datepicker" style="width: 74px;" value="<?= date('d.m.Y', time() + 3600*24*3) ?>">
-        <input name="before" type="text" class="datepicker" style="width: 75px;" value="<?= date('d.m.Y', time() + 3600*24*10) ?>">
+        <input name="before" type="text" class="datepicker" style="width: 74px;" value="<?= date('d.m.Y', time() + 3600*24*10) ?>">
         
         Количество ночей:<br/>
         <input name="nightsMin" type="text" style="width: 74px;" value="6">
-        <input name="nightsMax" type="text" style="width: 75px;" value="14">
+        <input name="nightsMax" type="text" style="width: 74px;" value="14">
       </div>  
       <div>
         Валюта:<br/>
-        <select name="currency" style="width: 180px">
+        <select name="currency" style="width: 179px">
           <option value="5561">Доллар США</option>
           <option value="8390" selected="selected">Рубль</option>
           <option value="18864">Евро</option>
@@ -36,7 +36,7 @@
         
         Диапазон цен:<br/>
         <input name="priceMin" type="text" style="width: 74px;" value="0">
-        <input name="priceMax" type="text" style="width: 75px;" value="999999">
+        <input name="priceMax" type="text" style="width: 74px;" value="999999">
       </div>
 <!-- -->
       
--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <title>Untitled</title>
+  <title>{{ isset($title) ? $title : 'Поиск туров' }}</title>
   
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
   <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
